fe478f9f69dfd7e8441ea99fc827207a72895556
Amador-Rodriguez <amador.rodriguezbs@uanl.edu.mx>
2022-10-22 23:34:36 -0500

    Pantalla de transacciones y login

--- app/src/main/java/com/example/linventario/TransactionsFragment.kt
+++ app/src/main/java/com/example/linventario/TransactionsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.linventario
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.linventario.databinding.FragmentInventoryBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +35,16 @@ class TransactionsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val bind = FragmentInventoryBinding.inflate(layoutInflater)
+        bind.btnNew.setOnClickListener{
+            val intent = Intent (activity, newTransactionActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transactions, container, false)
+        return bind.root
     }
 
     companion object {
---
+++ app/src/main/java/com/example/linventario/newTransactionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.linventario
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class newTransactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_transaction)
+    }
+}
